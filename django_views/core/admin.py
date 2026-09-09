b198d690cb9a16057e10db6465ff04d95c6d8440
Júlio Orlando <falecomjulioorlando@outlook.com>
2026-09-09 10:29:15 -0300

    revert: mantém exercício anterior sem alterações do admin

--- django_views/core/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-
-# O Django Admin já disponibiliza a administração de usuários e grupos
-# por meio de django.contrib.auth quando o painel administrativo está ativo.
