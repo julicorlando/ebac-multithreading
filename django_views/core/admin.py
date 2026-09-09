123a40ae907bd00a77d7c3af7a9eb4ee2cbf7625
Júlio Orlando <falecomjulioorlando@outlook.com>
2026-09-09 10:28:57 -0300

    feat: adiciona estrutura do Django Admin

---
+++ django_views/core/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+
+# O Django Admin já disponibiliza a administração de usuários e grupos
+# por meio de django.contrib.auth quando o painel administrativo está ativo.
