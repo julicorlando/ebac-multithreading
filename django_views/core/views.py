90bf86f04fd4d5887dade4a3e359e29b090cd5aa
Júlio Orlando <falecomjulioorlando@outlook.com>
2026-09-09 10:57:54 -0300

    feat: adiciona views com templates do portfólio

--- django_views/core/views.py
+++ django_views/core/views.py
@@ -1,5 +1,13 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Post
 
 
 def home(request):
-    return HttpResponse("Hello World")
+    posts = Post.objects.filter(published=True)
+    return render(request, "core/index.html", {"posts": posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk, published=True)
+    return render(request, "core/detail.html", {"post": post})
